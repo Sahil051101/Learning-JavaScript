3eff1fb498143a16f4cab6174bf3895edb244972
Sahil Agrawal <88614348+Sahil051101@users.noreply.github.com>
2024-04-02 13:26:49 +0000

    Conversion of datatypes

---
+++ 01_basics/ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = 33
+console.log(typeof(score))
+
+let score2 = "33abc"
+console.log(typeof(score2))
+
+let num = Number(score2)
+console.log(typeof(num))
+console.log(num)
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 and false -> 0
+
+let isLoggedIn = "" ;
+let booliLi = Boolean(isLoggedIn)
+console.log(booliLi)
+console.log(typeof(booliLi))
+
+// 1 -> true and 0 -> false
+// "" -> false
+// "value" -> true
+
+let someNumber = 33 
+let stringsn = String(someNumber)
+console.log(someNumber)
+console.log(typeof(stringsn))
